1a6143c7d43bb9fc4ac6de16e3b6d14885936787
junsuPark <krpjs0508@gmail.com>
2023-10-08 22:39:48 +0900

    scr

---
+++ app/src/main/java/app/junsu/composedestinationexample/ui/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package app.junsu.composedestinationexample.ui.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+) {
+
+}
---
+++ app/src/main/java/app/junsu/composedestinationexample/ui/login/LoginScreen.kt
@@ -0,0 +1,13 @@
+package app.junsu.composedestinationexample.ui.login
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier,
+) {
+
+}
