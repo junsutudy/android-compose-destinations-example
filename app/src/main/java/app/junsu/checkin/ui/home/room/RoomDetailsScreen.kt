a870cc6b3b65f2c99ae7721d66251bc03cc851f1
junsuPark <krpjs0508@gmail.com>
2023-10-08 23:21:59 +0900

    room detail

--- app/src/main/java/app/junsu/checkin/ui/home/room/RoomDetailsScreen.kt
+++ app/src/main/java/app/junsu/checkin/ui/home/room/RoomDetailsScreen.kt
@@ -1,16 +1,15 @@
-package app.junsu.checkin.ui.room
+package app.junsu.checkin.ui.home.room
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import app.junsu.checkin.data.model.Room
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination
 @Composable
 fun RoomDetailsScreen(
     modifier: Modifier = Modifier,
-    roomNumber: Int,
-    roomName: String,
-    using: Boolean,
+    room: Room,
 ) {
 
 }
