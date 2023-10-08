c3cceb361cfa7addccc74020bc5456f79f42e54a
junsuPark <krpjs0508@gmail.com>
2023-10-08 23:00:54 +0900

    room

---
+++ app/src/main/java/app/junsu/composedestinationexample/ui/room/RoomDetailsScreen.kt
@@ -0,0 +1,16 @@
+package app.junsu.composedestinationexample.ui.room
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun RoomDetailsScreen(
+    modifier: Modifier = Modifier,
+    roomNumber: Int,
+    roomName: String,
+    using: Boolean,
+) {
+
+}
