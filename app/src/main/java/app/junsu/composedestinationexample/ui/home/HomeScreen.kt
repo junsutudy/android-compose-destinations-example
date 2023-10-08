a102f2eea565bec529abd3979f33fb3bcb6ab857
junsuPark <krpjs0508@gmail.com>
2023-10-08 22:55:34 +0900

    annot

--- app/src/main/java/app/junsu/composedestinationexample/ui/home/HomeScreen.kt
+++ app/src/main/java/app/junsu/composedestinationexample/ui/home/HomeScreen.kt
@@ -3,11 +3,15 @@ package app.junsu.composedestinationexample.ui.home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@RootNavGraph(start = true)
 @Destination
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
+    navigator: DestinationsNavigator,
 ) {
 
 }
