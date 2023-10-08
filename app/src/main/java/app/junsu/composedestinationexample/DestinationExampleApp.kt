d9dd48af848c4ffad985f7134c381790c611edc7
junsuPark <krpjs0508@gmail.com>
2023-10-08 22:47:41 +0900

    app

---
+++ app/src/main/java/app/junsu/composedestinationexample/DestinationExampleApp.kt
@@ -0,0 +1,7 @@
+package app.junsu.composedestinationexample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DestinationExampleApp : Application()
