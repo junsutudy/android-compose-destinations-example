73e68c8e04af733a588082d309fa6bdef5dc52a3
junsuPark <krpjs0508@gmail.com>
2023-10-09 10:25:52 +0900

    bottom bar

--- app/src/main/java/app/junsu/checkin/ui/home/BottomAppBar.kt
+++ app/src/main/java/app/junsu/checkin/ui/home/BottomAppBar.kt
@@ -1,8 +1,58 @@
 package app.junsu.checkin.ui.home
 
 import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
+import app.junsu.checkin.R
+import app.junsu.checkin.ui.destinations.DirectionDestination
+import app.junsu.checkin.ui.destinations.ExploreScreenDestination
+import app.junsu.checkin.ui.destinations.RoomListScreenDestination
+
+class BottomAppBarIcons(
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+)
+
+@Composable
+fun CheckInBottomAppBar(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+) {
+    BottomAppBar(
+        modifier = modifier,
+    ) {
+
+    }
+}
+
+enum class CheckInSections(
+    val direction: DirectionDestination,
+    val icon: BottomAppBarIcons,
+    @StringRes val label: Int,
+) {
+    EXPLORE(
+        direction = ExploreScreenDestination,
+        icon = BottomAppBarIcons(
+            selectedIcon = Icons.Filled.LocationOn,
+            unselectedIcon = Icons.Outlined.LocationOn,
+        ),
+        label = R.string.bottom_app_bar_section_explore,
+    ),
+    ROOM_LIST(
+        direction = RoomListScreenDestination,
+        icon = BottomAppBarIcons(
+            selectedIcon = Icons.Filled.Search,
+            unselectedIcon = Icons.Outlined.Search,
+        ),
+        label = R.string.bottom_app_bar_section_room_list,
+    ),
+    ;
+}
---
+++ app/src/main/java/app/junsu/checkin/ui/home/_HomeScreen.kt
@@ -0,0 +1,29 @@
+package app.junsu.checkin.ui.home
+
+/*
+interface HomeNavigator {
+    fun openHome()
+    fun openExplore()
+    fun openRoomList()
+    fun openRoomDetails(room: Room)
+    fun openAppInformation(version: Int)
+}
+
+@RootNavGraph(start = true)
+@Destination
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    // navigator: HomeNavigator,
+) {
+    Scaffold(
+        modifier = modifier,
+    ) { padValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padValues)
+                .background(Color.Blue),
+        ) {}
+    }
+}*/
